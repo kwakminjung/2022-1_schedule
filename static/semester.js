7df467d0499b1997717a197a7ea00946c512c29b
kwakminjung <kwak1126mj@gmail.com>
2022-05-07 18:05:51 +0900

    semester

---
+++ static/semester.js
@@ -0,0 +1,39 @@
+var writingbasic = document.getElementById("writingbasic");
+writingbasic.addEventListener('click',function(){ 
+   alert(writingbasic.textContent);  //writingbasic에 적혀있는 값을 알림창에 띄워줌
+});
+
+var cprogramming = document.getElementById("cprogramming");
+cprogramming.addEventListener('click',function(){ 
+   alert(cprogramming.textContent);
+});
+
+var statistic = document.getElementById("statistic");
+statistic.addEventListener('click',function(){ 
+   alert(statistic.textContent);
+});
+
+var computerbasic1 = document.getElementById("computerbasic");
+computerbasic1.addEventListener('click',function(){ 
+   alert(computerbasic1.textContent);
+});
+
+// var computerbasic2 = document.getElementById("computerbasic2");
+// computerbasic2.addEventListener('click',function(){ 
+//    alert(computerbasic2.textContent);
+// });
+
+var commuengSW = document.getElementById("commuengSW");
+commuengSW.addEventListener('click',function(){ 
+   alert(commuengSW.textContent);
+});
+
+var commuengLR = document.getElementById("commuengLR");
+commuengLR.addEventListener('click',function(){ 
+   alert(commuengLR.textContent);
+});
+
+var digitalliterecy = document.getElementById("digitalliterecy");
+digitalliterecy.addEventListener('click',function(){ 
+   alert(digitalliterecy.textContent);
+});
